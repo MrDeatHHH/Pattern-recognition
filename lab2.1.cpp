a535dab663aa86b41ccb2a42123f4daad908dab3
MrDeatHHH <37218089+MrDeatHHH@users.noreply.github.com>
2019-10-29 23:10:15 +0200

    Task is completed
    We showed that out hybrid stratagy is adaptive to both risks. Such behavior is the result of q3 depending on parameter alpha, which can be changed. If alpha -> -|G|, G >> 1, then q3 takes argmin p(k|x), if alpha -> +|G|, G >> 1, then q3 takes argmax p(k|x). So using parameter alpha we can go from one marginal case to another, thus going throw a lot of possible strategies. Tests are performed for every strategy (with few alpha values) and every risk.

--- lab2.1.cpp
+++ lab2.1.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include <iostream>
 #include <fstream>
 #include <ctime>
@@ -72,19 +71,24 @@ double R1q2(const int X, const int K, double **PKX)
     double R = 0;
     double px = 1. / double(X);
     double Rmin = quiteALot;
-    for (int k_ = 0; k_ < K; ++k_)
+    double Rfull = 0;
+    for (int i = 0; i < X; ++i)
     {
-        R = 0;
-        for (int i = 0; i < X; ++i)
+        Rmin = quiteALot;
+        for (int k_ = 0; k_ < K; ++k_)
         {
+            R = 0;
+
             for (int j = 0; j < K; ++j)
                 R += PKX[j][i] * px * pow(k_ - j, 2);
+
+            if (Rmin > R)
+                Rmin = R;
         }
-        if (Rmin > R)
-            Rmin = R;
+        Rfull += Rmin;
     }
 
-    return Rmin;
+    return Rfull;
 }
 
 int q3(const int x, const int K, double** PKX, const float alpha)
@@ -120,30 +124,153 @@ double R1q3(const int X, const int K, double **PKX, const float alpha)
     return R;
 }
 
-
-int main()
+double R2q1(const int X, const int K, double **PKX)
 {
-    srand((unsigned int)(time(0)));
+    double R = 0;
+    double px = 1. / double(X);
+    for (int i = 0; i < X; ++i)
+    {
+        R += PKX[q1(i, K, PKX)][i] * px;
+    }
 
-    const int X = 2;
-    const int K = 5;
+    return 1 - R;
+}
+
+double R2q2(const int X, const int K, double **PKX)
+{
+    double R = 0;
+    double px = 1. / double(X);
+    double Rmin = quiteALot;
+    double Rfull = 0;
+    for (int i = 0; i < X; ++i)
+    {
+        Rmin = quiteALot;
+        for (int k_ = 0; k_ < K; ++k_)
+        {
+            R = PKX[k_][i] * px;
+
+            if (Rmin > R)
+                Rmin = R;
+        }
+        Rfull += Rmin;
+    }
+
+    return 1 - Rfull;
+}
+
+double R2q3(const int X, const int K, double **PKX, const float alpha)
+{
+    double R = 0;
+    double px = 1. / double(X);
+    for (int i = 0; i < X; ++i)
+    {
+        R += PKX[q3(i, K, PKX, alpha)][i] * px;
+    }
+
+    return 1 - R;
+}
+
+void genXK(int &X, int &K, double **&PKX)
+{
+    cout << "Enter X, K" << endl;
+    cin >> X;
+    cin >> K;
+    PKX = genPKX(X, K);
+}
+
+void test()
+{
+    int X = 2;
+    int K = 3;
 
     double **PKX = genPKX(X, K);
 
+    PKX[0][0] = 0.5;
+    PKX[1][0] = 0.3;
+    PKX[2][0] = 0.2;
+    PKX[0][1] = 0.7;
+    PKX[1][1] = 0.3;
+    PKX[2][1] = 0.0;
+    
+    assert(R1q1(X, K, PKX) > 0.7 - epsilon);
+    assert(R1q1(X, K, PKX) < 0.7 + epsilon);
+    assert(R1q2(X, K, PKX) > 0.5 - epsilon);
+    assert(R1q2(X, K, PKX) < 0.5 + epsilon);
+    assert(R1q3(X, K, PKX, 1000) > 0.7 - epsilon);
+    assert(R1q3(X, K, PKX, 1000) < 0.7 + epsilon);
+    assert(R1q3(X, K, PKX, -1000) > 2.7 - epsilon);
+    assert(R1q3(X, K, PKX, -1000) < 2.7 + epsilon);
+    assert(R1q3(X, K, PKX, 0) > 0.5 - epsilon);
+    assert(R1q3(X, K, PKX, 0) < 0.5 + epsilon);
+    assert(R1q3(X, K, PKX, 10) > 0.7 - epsilon);
+    assert(R1q3(X, K, PKX, 10) < 0.7 + epsilon);
+    assert(R1q3(X, K, PKX, -10) > 1.9 - epsilon);
+    assert(R1q3(X, K, PKX, -10) < 1.9 + epsilon);
+    assert(R2q1(X, K, PKX) > 0.4 - epsilon);
+    assert(R2q1(X, K, PKX) < 0.4 + epsilon);
+    assert(R2q2(X, K, PKX) > 0.9 - epsilon);
+    assert(R2q2(X, K, PKX) < 0.9 + epsilon);
+    assert(R2q3(X, K, PKX, 1000) > 0.4 - epsilon);
+    assert(R2q3(X, K, PKX, 1000) < 0.4 + epsilon);
+    assert(R2q3(X, K, PKX, -1000) > 0.9 - epsilon);
+    assert(R2q3(X, K, PKX, -1000) < 0.9 + epsilon);
+    assert(R2q3(X, K, PKX, 0) > 0.5 - epsilon);
+    assert(R2q3(X, K, PKX, 0) < 0.5 + epsilon);
+    assert(R2q3(X, K, PKX, 10) > 0.4 - epsilon);
+    assert(R2q3(X, K, PKX, 10) < 0.4 + epsilon);
+    assert(R2q3(X, K, PKX, -10) > 0.85 - epsilon);
+    assert(R2q3(X, K, PKX, -10) < 0.85 + epsilon);
+}
+
+void printPKX(const int X, const int K, double **PKX)
+{
     for (int i = 0; i < X; ++i)
     {
         for (int j = 0; j < K; ++j)
             cout << PKX[j][i] << " ";
         cout << endl;
     }
+}
 
-    cout << q1(0, K, PKX) << endl;
+int main()
+{
+    srand((unsigned int)(time(0)));
 
-    cout << "R1(q1) = " << R1q1(X, K, PKX) << endl;
-    cout << "R1(q2) = " << R1q2(X, K, PKX) << endl;
-    cout << "R1(q3) = " << R1q3(X, K, PKX, 0.1) << endl;
+    test();
+
+    int X = 100;
+    int K = 10;
+
+    double **PKX = genPKX(X, K);
+
+    /*
 
     for (int i = 0; i < X; ++i)
+        delete[] PKX[i];
+
+    delete[] PKX;
+
+    genXK(X, K, PKX);
+    */
+    
+    //printPKX(X, K, PKX);
+
+    cout << "R1(q1) =                " << R1q1(X, K, PKX) << endl << endl;
+    cout << "R1(q2) =                " << R1q2(X, K, PKX) << endl << endl;
+    cout << "R1(q3, alpha =  1000) = " << R1q3(X, K, PKX, 1000) << endl << endl;
+    cout << "R1(q3, alpha = -1000) = " << R1q3(X, K, PKX, -1000) << endl << endl;
+    cout << "R1(q3, alpha =  0   ) = " << R1q3(X, K, PKX, 0) << endl << endl;
+    cout << "R1(q3, alpha =  10  ) = " << R1q3(X, K, PKX, 10) << endl << endl;
+    cout << "R1(q3, alpha = -10  ) = " << R1q3(X, K, PKX, -10) << endl << endl;
+    cout << "R2(q1) =                " << R2q1(X, K, PKX) << endl << endl;
+    cout << "R2(q2) =                " << R2q2(X, K, PKX) << endl << endl;
+    cout << "R2(q3, alpha =  1000) = " << R2q3(X, K, PKX, 1000) << endl << endl;
+    cout << "R2(q3, alpha = -1000) = " << R2q3(X, K, PKX, -1000) << endl << endl;
+    cout << "R2(q3, alpha =  0   ) = " << R2q3(X, K, PKX, 0) << endl << endl;
+    cout << "R2(q3, alpha =  10  ) = " << R2q3(X, K, PKX, 10) << endl << endl;
+    cout << "R2(q3, alpha = -10  ) = " << R2q3(X, K, PKX, -10) << endl << endl;
+
+    for (int i = 0; i < K; ++i)
         delete[] PKX[i];
 
     delete[] PKX;
