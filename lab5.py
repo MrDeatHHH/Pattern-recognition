201ba710ae910485f28e152d4562389f5effdadc
MrDeatHHH <37218089+MrDeatHHH@users.noreply.github.com>
2020-01-02 15:37:38 +0200

    third step of EM algorithm abd tests

--- lab5.py
+++ lab5.py
@@ -53,3 +53,62 @@ for it in range(max_iter):
         for m in range(K):
             p_x_k[n][m] = np.prod((p_k_i_j[m] ** x01_train[n]) *
                                   ((1 - p_k_i_j[m]) ** (1 - x01_train[n])))
+
+    # 3
+    for n in range(X):
+        for m in range(K):
+            p_k_x[m][n] = p_x_k[n][m] * p_k[m] / np.sum(p_x_k[n] * p_k)
+# Results
+print('Classes')
+for i in range(2):
+    plt.imshow(p_k_i_j[i])
+    plt.show()
+
+for i in range(K):
+    
+    norm = np.sum(p_k_i_j[i])       
+    p_k_i_j[i] /= norm
+# Determining which class is which number
+if (np.prod((p_k_i_j[0] ** x01_train[0]) *
+            ((1 - p_k_i_j[0]) ** (1 - x01_train[0]))) >
+    np.prod((p_k_i_j[1] ** x01_train[0]) *
+            ((1 - p_k_i_j[1]) ** (1 - x01_train[0])))) and y01_train[0] == 1:
+    swap = 1
+
+if (np.prod((p_k_i_j[0] ** x01_train[0]) *
+            ((1 - p_k_i_j[0]) ** (1 - x01_train[0]))) <
+    np.prod((p_k_i_j[1] ** x01_train[0]) *
+            ((1 - p_k_i_j[1]) ** (1 - x01_train[0])))) and y01_train[0] == 0:
+    swap = 1
+
+print('Swap = ', swap)
+# Tests
+print('Tests')
+answers = np.zeros(x01_test.shape[0], y01_test.dtype)
+for i in range(x01_test.shape[0]):
+    a1 = np.logical_and((p_k_i_j[0] == 0), (x01_test[i] == 1))
+    a2 = np.sum(a1)
+    b1 = np.logical_and((p_k_i_j[1] == 0), (x01_test[i] == 1))
+    b2 = np.sum(b1)
+    if (a2 > 0):
+        if (a2 > b2):
+            answers[i] = swap
+        else:
+            answers[i] = abs(swap - 1)
+    else:
+        if (b2 > a2):
+            answers[i] = abs(swap - 1)
+        else:
+            a = np.sum((p_k_i_j[0] ** x01_test[i]) * ((1 - p_k_i_j[0]) ** (1 - x01_test[i])))
+            b = np.sum((p_k_i_j[1] ** x01_test[i]) * ((1 - p_k_i_j[1]) ** (1 - x01_test[i])))
+            if (a > b):
+                answers[i] = swap                
+            else:
+                answers[i] = abs(swap - 1)
+
+mask = (answers == y01_test)
+        
+unique, counts = np.unique(mask, return_counts = True)
+
+perc = (counts[0] if unique[0] else counts[1]) / np.sum(counts)
+print('Accuracy = ', perc)
